467154b88c4c013f153a8a7d203de42772cf9155
Andrew Pham <66355109+adp11@users.noreply.github.com>
2022-11-03 16:21:33 -0400

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -14,7 +14,12 @@ public:
   keyType key;
   vector<Node> neighbors;
 
-  Node(dataType data, keyType key, vector<keyType> edges) {
+  Node(dataType data, keyType key) {
+    data = data;
+    key = key;
+  }
+
+  Node(dataType data, keyType key, vector<Node> edges) {
     // create node with neighbors
     data = data;
     key = key;
@@ -35,10 +40,29 @@ public:
     // create graph out of edges
     int n = data.size();
     graph.reserve(n);
+    map<keyType, Node> m; // map key to the node that contains this key
+    vector<vector<Node>> newEdges; 
+    newEdges.reserve(n);
+    
+    for (int i = 0; i < n; i++) { // create all nodes and store their pointers
+      Node<dataType, keyType> *newNode = new Node<dataType, keyType> (data[i], keys[i]);
+      graph[i] = newNode;
+      m[keys[i]] = newNode;
+    }
+
+    for (int i = 0; i < edges.size(); i++) { // map all edges of keys into edges of nodes
+      vector<keyType> neighbors = edges[i]sad
+      for (int j = 0; j < neighbors.size(); j++) {
+        newEdges[i].reserve(neighbors.size());
+        newEdges[i][j] = m[edges[i][j]];
+      }
+    }
 
     for (int i = 0; i < n; i++) {
-      Node<dataType, keyType> *newNode = new Node<dataType, keyType> (data[i], keys[i], edges[i]);
-      graph[i] = newNode;
+      vector<Node> neighbors = newEdges[i];
+      for (int j=0; j > neighbors.size(); j++) {
+        add_edge()
+      }
     }
   }
 
@@ -46,7 +70,7 @@ public:
     // deallocate
   }
 
-  Node get(keyType k);
+  // Node<dataType, keyType> get(keyType k);
   bool reachable(keyType u, keyType v);
   void bfs(keyType s);
   void print_path(keyType u, keyType v);
